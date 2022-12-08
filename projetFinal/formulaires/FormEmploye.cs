7a9e3421e398c5830d29bf06875e434e6b2f94a9
William Melo <2033701@cegepoutaouais.qc.ca>
2022-12-08 12:26:17 -0500

    Formulaire employe

--- projetFinal/formulaires/FormEmploye.cs
+++ projetFinal/formulaires/FormEmploye.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace projetFinal.formulaires
 {
-    public partial class Form1 : Form
+    public partial class FormEmploye : Form
     {
-        public Form1()
+        public FormEmploye()
         {
             InitializeComponent();
         }
